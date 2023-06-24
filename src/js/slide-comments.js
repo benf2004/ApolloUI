784037b0b51eaf2e0f780448fb22c14ba2a3367f
benf2004 <12115686+benf2004@users.noreply.github.com>
2023-06-23 20:36:38 -0600

    post and comment styling

--- src/js/slide-comments.js
+++ src/js/slide-comments.js
@@ -1,10 +1,15 @@
 const list = document.querySelector(".comments")
 new Slip(list)
+let lastSwipeDistance = 1000; // any number greater than threshold
+const thresholdVal = 350; // in px
 
 list.addEventListener('slip:beforereorder', function(e) {
     e.preventDefault();
 });
 
+list.addEventListener('slip:beforeswipe', function(e) {
+    removeActions()
+});
 
 list.addEventListener('slip:swipe', function (e){
     removeActions()
@@ -21,17 +26,48 @@ function removeActions(){
 list.addEventListener('slip:cancelswipe', removeActions)
 
 list.addEventListener('slip:leftaction', function (e){
+    let ra = document.querySelector(".right-action")
+    if (ra) ra.remove()
     const a = e.target.getBoundingClientRect()
     let la = document.querySelector(".left-action")
     if (!la) {
         const leftActionMenu =
             `<div class="left-action flex">
-            <img class="icon-white" src="" height="50px;">
+            <img class="icon-white" alt="up arrow" src="../icons/arrowup.svg" style="height: 50px">
             </div>`
         e.target.insertAdjacentHTML("beforebegin", leftActionMenu)
         la = document.querySelector(".left-action")
     }
-    la.style = `width:${a.x}px; z-index: 99; height: ${a.height}px; position: fixed; top: ${a.top}px; left: 0; margin-right: 50px; align-items: center; justify-content: center`
-    la.style.backgroundColor = e.detail.transformVal > 250 ? "#585FDB" : "#EC682C"
-    la.querySelector("img").src = e.detail.transformVal > 250 ? "../icons/arrowdown.svg" : "../icons/arrowup.svg"
+    la.style = `width:${a.x}px; z-index: 99; height: ${a.height}px; position: fixed; top: ${a.top}px; left: 0; align-items: center; justify-content: center`
+    const isFarLeftAction = e.detail.transformVal > thresholdVal;
+    la.style.backgroundColor = isFarLeftAction ? "#585FDB" : "#EC682C"
+    if ((lastSwipeDistance > thresholdVal && e.detail.transformVal < thresholdVal) || (lastSwipeDistance < thresholdVal && e.detail.transformVal > thresholdVal)){
+        la.querySelector("img").src = isFarLeftAction ? "../icons/arrowdown.svg" : "../icons/arrowup.svg"
+        la.querySelector("img").alt = isFarLeftAction ? "down arrow" : "up arrow"
+    }
+    lastSwipeDistance = e.detail.transformVal
+})
+
+list.addEventListener("slip:rightaction", function (e){
+    let la = document.querySelector(".left-action")
+    if (la) la.remove()
+
+    const a = e.target.getBoundingClientRect()
+    let ra = document.querySelector(".right-action")
+    if (!ra) {
+        const rightActionMenu =
+            `<div class="right-action flex">
+            <img class="icon-white" alt="collapse" src="../icons/chevronup.svg" style="height: 50px; max-width: 50px;">
+            </div>`
+        e.target.insertAdjacentHTML("beforebegin", rightActionMenu)
+        ra = document.querySelector(".right-action")
+    }
+    ra.style = `width:${a.x * -1}px; z-index: 99; height: ${a.height}px; position: fixed; top: ${a.top}px; right: 0; align-items: center; justify-content: center`
+    const isFarAction = e.detail.transformVal > thresholdVal;
+    ra.style.backgroundColor = isFarAction ? "#57B2F9" : "#3578F7"
+    if ((lastSwipeDistance > thresholdVal && e.detail.transformVal < thresholdVal) || (lastSwipeDistance < thresholdVal && e.detail.transformVal > thresholdVal)){
+        ra.querySelector("img").src = isFarAction ? "../icons/reply.svg" : "../icons/chevronup.svg"
+        ra.querySelector("img").alt = isFarAction ? "down arrow" : "up arrow"
+    }
+    lastSwipeDistance = e.detail.transformVal
 })
--- src/js/slide-posts.js
+++ src/js/slide-posts.js
@@ -1,18 +1,73 @@
 const list = document.querySelector("main")
 new Slip(list)
+let lastSwipeDistance = 1000; // any number greater than threshold
+const thresholdVal = 350; // in px
 
 list.addEventListener('slip:beforereorder', function(e) {
     e.preventDefault();
 });
 
+list.addEventListener('slip:beforeswipe', function(e) {
+    removeActions()
+});
+
 list.addEventListener('slip:swipe', function (e){
+    removeActions()
     e.preventDefault();
 })
 
+function removeActions(){
+    let la = document.querySelector(".left-action")
+    let ra = document.querySelector(".right-action")
+    if (la) la.remove()
+    if (ra) ra.remove()
+}
+
+list.addEventListener('slip:cancelswipe', removeActions)
+
 list.addEventListener('slip:leftaction', function (e){
+    let ra = document.querySelector(".right-action")
+    if (ra) ra.remove()
     const a = e.target.getBoundingClientRect()
-    const leftActionMenu = `<div class="left-action" style="width:a.w; height: ${e.target.style.height}"></div>`
-    e.target.insertAdjacentHTML("beforebegin", leftActionMenu)
-    const da = document.querySelector(".left-action")
-    da.style.borderRight = `${e.detail.transformVal}px solid #EC682C`
+    let la = document.querySelector(".left-action")
+    if (!la) {
+        const leftActionMenu =
+            `<div class="left-action flex">
+            <img class="icon-white" alt="up arrow" src="../icons/arrowup.svg" style="height: 50px">
+            </div>`
+        e.target.insertAdjacentHTML("beforebegin", leftActionMenu)
+        la = document.querySelector(".left-action")
+    }
+    la.style = `width:${a.x}px; z-index: 99; height: ${a.height}px; position: fixed; top: ${a.top}px; left: 0; align-items: center; justify-content: center`
+    const isFarLeftAction = e.detail.transformVal > thresholdVal;
+    la.style.backgroundColor = isFarLeftAction ? "#585FDB" : "#EC682C"
+    if ((lastSwipeDistance > thresholdVal && e.detail.transformVal < thresholdVal) || (lastSwipeDistance < thresholdVal && e.detail.transformVal > thresholdVal)){
+        la.querySelector("img").src = isFarLeftAction ? "../icons/arrowdown.svg" : "../icons/arrowup.svg"
+        la.querySelector("img").alt = isFarLeftAction ? "down arrow" : "up arrow"
+    }
+    lastSwipeDistance = e.detail.transformVal
+})
+
+list.addEventListener("slip:rightaction", function (e){
+    let la = document.querySelector(".left-action")
+    if (la) la.remove()
+
+    const a = e.target.getBoundingClientRect()
+    let ra = document.querySelector(".right-action")
+    if (!ra) {
+        const rightActionMenu =
+            `<div class="right-action flex">
+            <img class="icon-white" alt="collapse" src="../icons/reply.svg" style="height: 50px; max-width: 50px;">
+            </div>`
+        e.target.insertAdjacentHTML("beforebegin", rightActionMenu)
+        ra = document.querySelector(".right-action")
+    }
+    ra.style = `width:${a.x * -1}px; z-index: 99; height: ${a.height}px; position: fixed; top: ${a.top}px; right: 0; align-items: center; justify-content: center`
+    const isFarAction = e.detail.transformVal > thresholdVal;
+    ra.style.backgroundColor = isFarAction ? "#56BB36" : "#57B2F9"
+    if ((lastSwipeDistance > thresholdVal && e.detail.transformVal < thresholdVal) || (lastSwipeDistance < thresholdVal && e.detail.transformVal > thresholdVal)){
+        ra.querySelector("img").src = isFarAction ? "../icons/bookmark.svg" : "../icons/reply.svg"
+        ra.querySelector("img").alt = isFarAction ? "down arrow" : "up arrow"
+    }
+    lastSwipeDistance = e.detail.transformVal
 })
